d9375bbfa4c385d1006c2236d95dd6671445e70a
Gabriel-Ivarsson <DingusDungus@github.com>
2023-01-17 13:55:34 +0100

    Worked on initial commit, code works now for demo

---
+++ code2vec.py
@@ -0,0 +1,21 @@
+import gensim
+import pandas as pd
+
+df = pd.read_json("data/Cell_Phones_and_Accessories_5.json", lines=True)
+print(df)
+df.shape
+
+review_text = df.reviewText.apply(gensim.utils.simple_preprocess)
+
+model = gensim.models.Word2Vec(
+    window=10,
+    min_count=2,
+    workers=4,
+)
+
+model.build_vocab(review_text, progress_per=1000)
+
+
+model.train(review_text, total_examples=model.corpus_count, epochs=model.epochs)
+print("Results for words found similar to \"awful\" ")
+print(model.wv.most_similar("awful"))
