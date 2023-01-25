101fdb8e3b21ec84c0f15d8b1f56a1d16626fca3
Noah Hakansson <noah.hakansson@protonmail.com>
2023-01-25 18:14:10 +0100

    Implemented a FastText version aswell, now trainModel.py and tool.py take a model `fast` or `w2v`

---
+++ tool.py
@@ -0,0 +1,42 @@
+import gensim
+from colors import green, red
+import sys
+
+def check_word_list(input_list: list[str], input_model: str):
+    if not input_list:
+        # if list is empty
+        print(red("Error: No words were provided as input"))
+        sys.exit(1)
+
+    if input_model == "fast":
+        model = gensim.models.fasttext.FastText.load("./word2vec-model/fast-model.bin")
+    elif input_model == "w2v":
+        model = gensim.models.word2vec.Word2Vec.load("./word2vec-model/w2v-model.bin")
+    else:
+        print("No valid model was given as second argv argument")
+        sys.exit(1)
+
+    for word in input_list:
+        print("Results for words found similar to \"" + word + "\"")
+        print(green(model.wv.most_similar(input_list)))
+
+    
+    print("Results for word that least fits in given word list")
+    print(red(model.wv.doesnt_match(input_list)))
+
+
+def main(argv: list[str]):
+    try:
+        input_model = argv[1]
+    except Exception as e:
+        print("Error: upply model as second argv param, 'w2v' for Word2Vec or 'fast' FastText")
+        raise e
+    input_list = argv[2:] # get all arguments, minus file name and model
+
+    # check against model
+    check_word_list(input_list, input_model)
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
--- word2vec-model/trainModel.py
+++ word2vec-model/trainModel.py
@@ -1,4 +1,6 @@
 import gensim
+from gensim.models.fasttext import FastText
+from gensim.test.utils import datapath
 import pandas as pd
 import sys
 import logging
@@ -7,19 +9,20 @@ if __name__ == '__main__':
 
     try:
         input_file = sys.argv[1]
+        input_model = sys.argv[2]
     except Exception as e:
-        print("Supply file as argv param, or 'lazy' to use pre-downloaded data.")
+        print("Error: Supply model as second argv param, 'w2v' for Word2Vec or 'fast' FastText")
         sys.exit(1)
 
-    if input_file == "lazy":
-        df = pd.read_json("./data/reviews_Cell_Phones_and_Accessories.json", lines=True)
+    if input_model == "fast":
+        df = pd.read_json(input_file, lines=True)
         print(df)
         df.shape
-        input_data = df.reviewText.apply(gensim.utils.simple_preprocess)
+        input_data = df.context.apply(gensim.utils.simple_preprocess)
         logging.info("Done reading data file!")
 
         # build vocabulary and train model
-        model = gensim.models.Word2Vec(
+        model = FastText(
             input_data,
             vector_size=150,
             window=10,
@@ -28,7 +31,8 @@ if __name__ == '__main__':
 
         model.build_vocab(input_data, progress_per=1000)
         model.train(input_data, total_examples=len(input_data), epochs=10)
-    else:
+        model.save("fast-model.bin")
+    elif input_model == "w2v":
         df = pd.read_json(input_file, lines=True)
         print(df)
         df.shape
@@ -45,5 +49,7 @@ if __name__ == '__main__':
 
         model.build_vocab(input_data, progress_per=1000)
         model.train(input_data, total_examples=model.corpus_count, epochs=model.epochs)
-
-    model.save("model.bin")
+        model.save("w2v-model.bin")
+    else:
+        print("Error: No valid model given")
+        sys.exit(1)
