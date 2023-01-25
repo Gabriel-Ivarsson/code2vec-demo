309ab627ea08ccf1d6953b38bc1162adaa17ee83
Gabriel-Ivarsson <DingusDungus@github.com>
2023-01-25 15:48:29 +0100

    Fixed a lot of the file structure, updated the data-preprocessor to take in directories as well :)

--- code2vec-demo.py
+++
@@ -1,7 +0,0 @@
-import gensim
-import sys
-input = sys.argv[1]
-model = gensim.models.Word2Vec.load("model.bin")
-
-print("Results for words found similar to \"" + input + "\"")
-print(model.wv.most_similar(input))
--- data/readme.md
+++
@@ -1,2 +0,0 @@
-# Link for initial demo-data
-http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Cell_Phones_and_Accessories_5.json.gz
