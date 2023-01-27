6e8d411711c5b8b7df1e42eb6fd6c9aebf2f3eb3
Gabriel-Ivarsson <DingusDungus@github.com>
2023-01-27 11:34:38 +0100

    Fixed so data-parser now has choice

--- data-preprocessing-GO/data-preprocessor.go
+++ data-preprocessing-GO/data-preprocessor.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-  "os" 
-  "github.com/Gabriel-Ivarsson/code2vec-demo/data-preprocessing-GO/src/ASTparser"
+	"fmt"
+	"os"
+
+	"github.com/Gabriel-Ivarsson/code2vec-demo/data-preprocessing-GO/src/ASTparser"
 )
 
 func main() {
-  dir := os.Args[1]
-  astParser.ParseDir(dir) 
-  return
+	dir := os.Args[1]
+	modelChoice := os.Args[2]
+	model, err := astParser.GetModel(modelChoice)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	astParser.ParseDir(model, dir)
+	return
 }
--- data-preprocessing-GO/src/ASTparser/Parser.go
+++ data-preprocessing-GO/src/ASTparser/Parser.go
@@ -1,6 +1,7 @@
 package astParser
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/parser"
@@ -8,87 +9,113 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
-  "regexp"
 )
 
-func sanitizeName(name string) string {
-  name = strings.ToLower(name)
-  name = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(name, "")
-  return name
+const (
+	word2vec = 1
+	fast     = 2
+)
+
+var (
+	funcType    = "funcDecl"
+	packageType = "package"
+)
+
+func GetModel(argv string) (int, error) {
+	if argv == "fast" {
+		return fast, nil
+	} else if argv == "w2v" {
+		return word2vec, nil
+	}
+
+	return 0, errors.New("Error; No valid model was given, valid: {\"w2v\", \"fast\"}")
 }
 
-func generateContext(node *ast.File) ([]string) {
-  var context []string
-  var parentPackage *ast.File
+func getTypeWord(model int, typeName string) string {
+	if model == word2vec {
+		return typeName
+	}
+	return ""
+}
 
-  ast.Inspect(node, func(n ast.Node) bool {
+func sanitizeName(name string) string {
+	name = strings.ToLower(name)
+	name = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(name, "")
+	return name
+}
 
-    switch x := n.(type) {
-      case *ast.File:
-        parentPackage = x     
-      case *ast.FuncDecl:
-    context = append(context, fmt.Sprintf("FuncDecl%s package%s", sanitizeName(x.Name.Name), sanitizeName(parentPackage.Name.Name)))
-    }
-    return true
-  })
-  return context 
+func generateContext(model int, node *ast.File) []string {
+	var context []string
+	var parentPackage *ast.File
+
+	ast.Inspect(node, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.File:
+			parentPackage = x
+		case *ast.FuncDecl:
+			context = append(context, fmt.Sprintf("%s%s %s%s", getTypeWord(model, "funcDecl"), getTypeWord(model, "package"), sanitizeName(x.Name.Name), sanitizeName(parentPackage.Name.Name)))
+		}
+		return true
+	})
+	return context
 }
 
 func context2json(contextList []string) []string {
-  var json []string
-  for i := 0;i < len(contextList);i++ {
-    jsonLine := "{\"context\":" + "\"" + contextList[i] + "\"}"
-    json = append(json, jsonLine)
-  }
-  return json
+	var json []string
+	for i := 0; i < len(contextList); i++ {
+		jsonLine := "{\"context\":" + "\"" + contextList[i] + "\"}"
+		json = append(json, jsonLine)
+	}
+	return json
 }
 
 func getGoFileNode(path string) (*ast.File, error) {
-  if filepath.Ext(path) == ".go" {
-    fset := token.NewFileSet()
+	if filepath.Ext(path) == ".go" {
+		fset := token.NewFileSet()
 
-    node, err := parser.ParseFile(fset, path, nil, 0)
-    if err != nil {
-      return nil, err
-    }
-    return node, nil
-  }
-  return nil, nil
+		node, err := parser.ParseFile(fset, path, nil, 0)
+		if err != nil {
+			return nil, err
+		}
+		return node, nil
+	}
+	return nil, nil
 }
 
 func write2DataFile(json []string, fileName string) error {
-  f, err := os.OpenFile(fileName,
-    os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-  if err != nil {
-    return err
-  }
-  
-  for i := 0;i < len(json);i++ {
-    if _, err := f.WriteString(string(json[i]) + "\n"); err != nil {
-      return err
-    }
-  }
-  
-  f.Close()
-  return nil
+	f, err := os.OpenFile(fileName,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(json); i++ {
+		if _, err := f.WriteString(string(json[i]) + "\n"); err != nil {
+			return err
+		}
+	}
+
+	f.Close()
+	return nil
 }
 
-func ParseDir(dir string) error {
-  err := filepath.Walk(dir, func (path string, info os.FileInfo, err error) error {
-    node, err := getGoFileNode(path)
-    if node != nil {
-      contextList := generateContext(node)
-      json := context2json(contextList) 
-      write2DataFile(json, "data.json")
-    } else {
-      if err != nil {
-        log.Println(err)
-      }
-    }
-    return nil
-  })
-  if err != nil {
-  }
-  return nil
+func ParseDir(model int, dir string) error {
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		node, err := getGoFileNode(path)
+		if node != nil {
+			contextList := generateContext(model, node)
+			json := context2json(contextList)
+			write2DataFile(json, "data.json")
+		} else {
+			if err != nil {
+				log.Println(err)
+			}
+		}
+		return nil
+	})
+	if err != nil {
+	}
+	return nil
 }
